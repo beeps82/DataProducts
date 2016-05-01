7a2ba2268f89f38a8682d175a2c553645df18b5b
beeps82 <eapen.binu@gmail.com>
2016-05-01 02:34:57 -0400

    added info

--- mtCarsApp/mtcars/ui.R
+++ mtCarsApp/mtcars/ui.R
@@ -4,9 +4,21 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Motor Trend Car Road Test"),
   
+  h5(('This report by Motor Trend magazine investigates the relationship between
+       a vehicle parameters and fuel consumption per gallon(MPG). This 
+       is from the 1974 MotorTrend US magazine. It compromises of 11 aspects of 
+        automobile design and performance for 32 automobiles(1973-74 models). 
+       We will investigate the factors like transmission, cylinders, displacement,
+       horse power, weight, qsec, gear and number of carburetors that contribute to performance. 
+       Also investigate the effects of car manufacturers. 
+       This app visualizes the individual effects of these parameters on vechicle performance in MPG.')),
+  
   # Sidebar 
   sidebarLayout(
     sidebarPanel(
+      
+
+      
       selectInput(inputId = "param", "Select a Parameter:",
                             c("Cylinders" = "cyl","Transmission" = "am", 
                               "Gears" = "gear",   "Carburetors" = "carb",
